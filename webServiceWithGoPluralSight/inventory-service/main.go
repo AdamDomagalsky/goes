dddab608a46809a4275a4e6ffe8fdae3de4f7142
Adam Domagalski <adadom@st.amu.edu.pl>
2022-10-06 17:36:03 +0200

    setup database and remove .json data and functionality

--- webServiceWithGoPluralSight/inventory-service/main.go
+++ webServiceWithGoPluralSight/inventory-service/main.go
@@ -4,12 +4,15 @@ import (
 	"log"
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql" // side effect for db, not calling implicitly
+	"goes/webServiceWithGoPluralSight/database"
 	"goes/webServiceWithGoPluralSight/product"
 )
 
 const apiBasePath = "/api"
 
 func main() {
+	database.SetupDatabase()
 	product.SetupRoute(apiBasePath)
 	log.Fatalln(http.ListenAndServe(":5005", nil))
 }
