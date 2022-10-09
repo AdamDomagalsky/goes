4b638ccd601c173d85eadc16df2fcb3aebbc5c80
Adam Domagalski <adadom@st.amu.edu.pl>
2022-10-09 14:48:36 +0200

    upload/download file endpoints

--- webServiceWithGoPluralSight/inventory-service/main.go
+++ webServiceWithGoPluralSight/inventory-service/main.go
@@ -7,6 +7,7 @@ import (
 	_ "github.com/go-sql-driver/mysql" // side effect for db, not calling implicitly
 	"goes/webServiceWithGoPluralSight/database"
 	"goes/webServiceWithGoPluralSight/product"
+	"goes/webServiceWithGoPluralSight/receipt"
 )
 
 const apiBasePath = "/api"
@@ -14,5 +15,6 @@ const apiBasePath = "/api"
 func main() {
 	database.SetupDatabase()
 	product.SetupRoute(apiBasePath)
+	receipt.SetupRoute(apiBasePath)
 	log.Fatalln(http.ListenAndServe(":5005", nil))
 }
