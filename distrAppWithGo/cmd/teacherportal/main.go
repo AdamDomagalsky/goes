677310b163400bffd007d8665ceb0718393644c0
Adam Domagalski <adadom@st.amu.edu.pl>
2022-10-13 14:45:15 +0200

    Status Monitoring Add hearbeatURL and client endpoint setup heartbeat on registry sync.Once every 3 sec Add gohtmls tempaltes and load tempaltes and for localhost:6000 teacher portal

--- distrAppWithGo/cmd/teacherportal/main.go
+++ distrAppWithGo/cmd/teacherportal/main.go
@@ -5,38 +5,42 @@ import (
 	"fmt"
 	stdlog "log"
 
-	"goes/distrAppWithGo/grades"
 	"goes/distrAppWithGo/log"
 	"goes/distrAppWithGo/registry"
 	"goes/distrAppWithGo/service"
+	"goes/distrAppWithGo/teacherportal"
 )
 
 func main() {
-	log.Run("./app.log")
-	host, port := "localhost", "6000"
-	serviceAddress := fmt.Sprintf("http://%v:%v", host, port)
-	var r registry.Registration
-	r.ServiceName = registry.GradingService
-	r.ServiceURL = serviceAddress
-	r.RequiredServices = []registry.ServiceName{registry.LogService}
-	r.ServiceUpdateURL = r.ServiceURL + "/services"
-	r.HeartbeatURL = r.ServiceURL + "/heartbeat"
-	ctx, err := service.Start(
-		context.Background(),
-		host,
-		port,
-		r,
-		grades.RegisterHandlers)
+	err := teacherportal.ImportTemplates()
 	if err != nil {
 		stdlog.Fatal(err)
 	}
 
+	host, portal := "localhost", "9000"
+	serviceAddress := fmt.Sprintf("http://%v:%v", host, portal)
+	var r registry.Registration
+	r.ServiceName = registry.TeacherPortal
+	r.ServiceURL = serviceAddress
+	r.RequiredServices = []registry.ServiceName{
+		registry.LogService,
+		registry.GradingService,
+	}
+	r.ServiceUpdateURL = r.ServiceURL + "/services"
+	r.HeartbeatURL = r.ServiceURL + "/heartbeat"
+	ctx, err := service.Start(context.Background(),
+		host,
+		portal,
+		r,
+		teacherportal.RegisterHandlers)
+	if err != nil {
+		stdlog.Fatal(err)
+	}
 	if logProvider, err := registry.GetProvider(registry.LogService); err == nil {
 		fmt.Printf("logging servcies found at: %v\n", logProvider)
 		log.SetClientLogger(logProvider, r.ServiceName)
 	}
 	stdlog.Printf("test if stdlog is beeing send to app.log")
-
 	<-ctx.Done()
-	fmt.Println("Shutting down grading service")
+	fmt.Println("Shutting down teacher portal")
 }
--- distrAppWithGo/registry/registration.go
+++ distrAppWithGo/registry/registration.go
@@ -5,7 +5,7 @@ type Registration struct {
 	ServiceURL       string
 	RequiredServices []ServiceName
 	ServiceUpdateURL string // client svcs. are gong to expose this endpoint
-
+	HeartbeatURL     string
 }
 
 type ServiceName string
@@ -13,6 +13,7 @@ type ServiceName string
 const (
 	LogService     = ServiceName("LogService")
 	GradingService = ServiceName("GradingService")
+	TeacherPortal  = ServiceName("TeacherPortal")
 )
 
 type patchEntry struct {
