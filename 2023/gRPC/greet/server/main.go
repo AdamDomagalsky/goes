8b0a9cebbe98a5941fb323b6930050ebf86eb355
AdamDomagalsky <adam.domagalski1994@gmail.com>
2023-07-07 18:02:02 +0200

    gRPC add self-signed SSL/TLS layer

--- 2023/gRPC/greet/server/main.go
+++ 2023/gRPC/greet/server/main.go
@@ -6,6 +6,7 @@ import (
 
 	pb "github.com/AdamDomagalsky/goes/2023/gRPC/greet/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 var addr string = "0.0.0.0:50051"
@@ -20,8 +21,18 @@ func main() {
 		log.Fatalf("Failed to listen on: %v\n", err)
 	}
 	log.Printf("Listening on %s\n", addr)
-
-	s := grpc.NewServer()
+	tls := true
+	options := []grpc.ServerOption{}
+	if tls {
+		certFile := "ssl/server.crt"
+		keyFile := "ssl/server.pem"
+		creds, err := credentials.NewServerTLSFromFile(certFile, keyFile)
+		if err != nil {
+			log.Fatalf("Fail loading certifcates: %v\n", err)
+		}
+		options = append(options, grpc.Creds(creds))
+	}
+	s := grpc.NewServer(options...)
 	pb.RegisterGreetServiceServer(s, &Server{})
 	if err != s.Serve(lis) {
 		log.Fatalf("Failed to serve %v\n", err)
