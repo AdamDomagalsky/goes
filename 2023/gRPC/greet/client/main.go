38ff72cca80e0d85b59d5ad222604d07810b1de6
Adam Domagalski <adam.domagalski@tacton.com>
2023-07-07 18:02:02 +0200

    gRPC add self-signed SSL/TLS layer

--- 2023/gRPC/greet/client/main.go
+++ 2023/gRPC/greet/client/main.go
@@ -2,28 +2,40 @@ package main
 
 import (
 	"log"
-	"time"
 
 	pb "github.com/AdamDomagalsky/goes/2023/gRPC/greet/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 var addr string = "0.0.0.0:50051"
 
 func main() {
-	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	tls := true
+	options := []grpc.DialOption{}
+	if tls {
+		certFile := "ssl/ca.crt"
+		creds, err := credentials.NewClientTLSFromFile(certFile, "")
+		if err != nil {
+			log.Fatalf("Error while loading CA trust certifcate: %v\n", err)
+		}
+		options = append(options, grpc.WithTransportCredentials(creds))
+	} else {
+		options = append(options, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	}
+	conn, err := grpc.Dial(addr, options...)
 	if err != nil {
 		log.Fatalf("Failed to connect: %v\n", err)
 	}
 	defer conn.Close()
 
 	c := pb.NewGreetServiceClient(conn)
-	// doGreet(c)
+	doGreet(c)
 	// doGreetManyTimes(c)
 	// doLongGreet(c)
 	// doGreetEveryone(c)
-	doGreetWithDeadline(c, 5*time.Second)
-	doGreetWithDeadline(c, 3*time.Second)
-	doGreetWithDeadline(c, 2*time.Second)
+	// doGreetWithDeadline(c, 5*time.Second)
+	// doGreetWithDeadline(c, 3*time.Second)
+	// doGreetWithDeadline(c, 2*time.Second)
 }
