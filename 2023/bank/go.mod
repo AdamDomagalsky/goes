e7a19b2775a738f9f46803e490c1f356f123062d
Adam Domagalski <adam.domagalski1994@gmail.com>
2023-08-18 16:26:37 +0200

    Implement jwt token maker

--- 2023/bank/go.mod
+++ 2023/bank/go.mod
@@ -5,7 +5,9 @@ go 1.20
 require (
 	github.com/gin-gonic/gin v1.9.1
 	github.com/go-playground/validator/v10 v10.14.0
+	github.com/golang-jwt/jwt/v5 v5.0.0
 	github.com/golang-migrate/migrate/v4 v4.16.2
+	github.com/google/uuid v1.3.0
 	github.com/lib/pq v1.10.9
 	github.com/ory/dockertest v3.3.5+incompatible
 	github.com/spf13/viper v1.16.0
---
+++ 2023/bank/token/jwt_maker.go
@@ -0,0 +1,90 @@
+package token
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+const minSecretKeySize = 32
+
+type JWTMaker struct {
+	secretKey string
+}
+
+func NewJWTMaker(secretKey string) (Maker, error) {
+
+	if len(secretKey) < minSecretKeySize {
+		return nil, fmt.Errorf("invalid key size: must be at least %d characters", minSecretKeySize)
+	}
+
+	return &JWTMaker{secretKey}, nil
+}
+
+func (maker *JWTMaker) CreateToken(username string, duration time.Duration) (string, error) {
+	payload, err := NewPayload(username, duration)
+	if err != nil {
+		return "", err
+	}
+
+	id := payload.ID.String()
+	if id == "" {
+		return "", ErrInvalidToken
+	}
+	jwtPayload := jwt.RegisteredClaims{
+		ID:        id,
+		Subject:   payload.Username,
+		IssuedAt:  jwt.NewNumericDate(payload.IssuedAt),
+		ExpiresAt: jwt.NewNumericDate(payload.ExpiresAt),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwtPayload)
+
+	signedToken, err := token.SignedString([]byte(maker.secretKey))
+	if err != nil {
+		return "", err
+	}
+	return signedToken, nil
+}
+func (maker *JWTMaker) VerifyToken(token string) (*Payload, error) {
+	keyFunc := func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, ErrInvalidToken
+		}
+		return []byte(maker.secretKey), nil
+	}
+
+	jwtToken, err := jwt.ParseWithClaims(token, &jwt.RegisteredClaims{}, keyFunc)
+	if err != nil {
+		if errors.Is(err, jwt.ErrTokenExpired) {
+			return nil, ErrExpiredToken
+		}
+
+		if errors.Is(err, jwt.ErrTokenUnverifiable) {
+			return nil, ErrInvalidToken
+		}
+
+		return nil, ErrInvalidToken
+	}
+	jwtPayload, ok := jwtToken.Claims.(*jwt.RegisteredClaims)
+	if !ok {
+		return nil, ErrInvalidToken
+	}
+
+	id, err := uuid.Parse(jwtPayload.ID)
+
+	if err != nil {
+		return nil, ErrInvalidToken
+
+	}
+	return &Payload{
+		ID:        id,
+		Username:  jwtPayload.Subject,
+		IssuedAt:  jwtPayload.IssuedAt.Time,
+		ExpiresAt: jwtPayload.ExpiresAt.Time,
+	}, nil
+}
