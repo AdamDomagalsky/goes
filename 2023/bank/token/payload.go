df0aa78eaf3c4503d63edee48b3105e30595dac0
Adam Domagalski <adam.domagalski1994@gmail.com>
2023-08-18 16:26:37 +0200

    Implement jwt token maker

---
+++ 2023/bank/token/payload.go
@@ -0,0 +1,35 @@
+package token
+
+import (
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+var (
+	ErrInvalidToken = errors.New("invalid token")
+	ErrExpiredToken = errors.New("expired token")
+)
+
+type Payload struct {
+	ID        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	IssuedAt  time.Time `json:"issued_at"`
+	ExpiresAt time.Time `json:"expires_at"`
+}
+
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	id, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+	payload := &Payload{
+		ID:        id,
+		Username:  username,
+		IssuedAt:  time.Now(),
+		ExpiresAt: time.Now().Add(duration),
+	}
+
+	return payload, nil
+}
